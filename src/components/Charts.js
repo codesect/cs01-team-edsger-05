5d8e8506e093cf18a719d1d1d14c199185917e60
bilal <bilal.soft000@gmail.com>
2019-11-20 11:59:36 +0500

    change chart component to dumb, remove state from charts

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,27 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Row, Col } from "reactstrap";
 import Piechart from "./Piechart";
 import Barchart from "./Barchart";
 
-class Charts extends Component {
-  render() {
-    return (
-      <Row style={{ paddingTop: "100px" }}>
-        <Col md="6">
-          <div className="heading" style={{ textAlign: "center" }}>
-            <h3>Category wise expense</h3>
-          </div>
-          <Piechart />
-        </Col>
-        <Col md="6">
-          <div className="heading" style={{ textAlign: "center" }}>
-            <h3>per day expense</h3>
-          </div>
-          <Barchart />
-        </Col>
-      </Row>
-    );
-  }
+export default function Charts() {
+  return (
+    <Row style={{ paddingTop: "100px" }}>
+      <Col md="6">
+        <div className="heading" style={{ textAlign: "center" }}>
+          <h3>Category wise expense</h3>
+        </div>
+        <Piechart />
+      </Col>
+      <Col md="6">
+        <div className="heading" style={{ textAlign: "center" }}>
+          <h3>per day expense</h3>
+        </div>
+        <Barchart />
+      </Col>
+    </Row>
+  );
 }
-
-export default Charts;
