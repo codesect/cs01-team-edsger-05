cb41a429b3fd4937d384036721e96d4734120c70
Akash Edakkalathur <akash20edakkalathur@gmail.com>
2019-11-24 09:02:12 +0530

    PR changes in expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
-import { Button, Form, FormGroup, Label ,Input} from "reactstrap";
-
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-const LoginSchema = Yup.object().shape({
+const ValidationSchema = Yup.object().shape({
   title: Yup.string().required("Required title"),
   amount: Yup.string().required("Required Amount"),
   category: Yup.string().required("Required category")
@@ -18,9 +16,6 @@ class ExpenseForm extends Component {
     };
   }
   render() {
-    if (this.state.isLoggedIn) {
-      return <Redirect to="/dashboard" />;
-    }
     return (
       <div
         style={{
@@ -40,11 +35,11 @@ class ExpenseForm extends Component {
           initialValues={{
             title: "",
             amount: "",
-            category:""
+            category: ""
           }}
-          validationSchema={LoginSchema}
+          validationSchema={ValidationSchema}
           onSubmit={({ setSubmitting }) => {
-           
+
           }}
         >
           {({
@@ -55,90 +50,87 @@ class ExpenseForm extends Component {
             handleBlur,
             values
           }) => (
-            <Form onSubmit={handleSubmit}>
-              <FormGroup>
-                <Label for="title">Title</Label>
-                <Field
-                  type="text"
-                  name="title"
-                  id="title"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.title}
-                  className={`form-control ${
-                    touched.title && errors.title ? "is-invalid" : ""
-                  }`}
-                  placeholder="Enter title"
-                />
-                <ErrorMessage
-                  component="div"
-                  name="title"
-                  className="invalid-feedback"
-                />
-              </FormGroup>
-                   <FormGroup>
-                <Label for="amount">Amount</Label>
-                <Field
-                  type="number"
-                  name="amount"
-                  id="amount"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.amount}
-                  className={`form-control ${
-                    touched.amount && errors.amount ? "is-invalid" : ""
-                  }`}
-                  placeholder="00.00"
-                />
-               
-                <ErrorMessage
-                  component="div"
-                  name="amount"
-                  className="invalid-feedback"
-                />
-              </FormGroup> 
-              <FormGroup>
-        <Label for="category">Category</Label>
-        <Input 
-        type="select" name="select" id="category"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        className={`form-control ${
-            touched.category && errors.category ? "is-invalid" : ""
-          }`}
-        >
-        <option value="" label="Select a category" />
-        <option value="Food" label="Food" />
-        <option value="Cloths" label="Cloths" />
-        <option value="Medical" label="Medical" />
-        
-        </Input>
-        
-        <ErrorMessage
-                  component="div"
-                  name="category"
-                  className="invalid-feedback"
-                />
-      </FormGroup>
-       <FormGroup>
-        <Label for="notes">Notes if any</Label>
-        <Input type="textarea" name="text" id="notes" />
-      </FormGroup>
-              <Button color="success" type="submit">
-                {this.props.buttonVal}
-              </Button>{" "}
-              <Button
-                color="success"
-                style={{
-                  backgroundColor: "white",
-                  border: "2px solid #03a679",
-                  color: "#03a679"
-                }}
-              >
-                Reset all
+              <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                  <Label for="title">Title</Label>
+                  <Field
+                    type="text"
+                    name="title"
+                    id="title"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.title}
+                    className={`form-control ${
+                      touched.title && errors.title ? "is-invalid" : ""
+                      }`}
+                    placeholder="Enter title"
+                  />
+                  <ErrorMessage
+                    component="div"
+                    name="title"
+                    className="invalid-feedback"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="amount">Amount</Label>
+                  <Field
+                    type="number"
+                    name="amount"
+                    id="amount"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.amount}
+                    className={`form-control ${
+                      touched.amount && errors.amount ? "is-invalid" : ""
+                      }`}
+                    placeholder="00.00"
+                  />
+                  <ErrorMessage
+                    component="div"
+                    name="amount"
+                    className="invalid-feedback"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="category">Category</Label>
+                  <Input
+                    type="select" name="select" id="category"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={`form-control ${
+                      touched.category && errors.category ? "is-invalid" : ""
+                      }`}
+                  >
+                    <option value="" label="Select a category" />
+                    <option value="Food" label="Food" />
+                    <option value="Cloths" label="Cloths" />
+                    <option value="Medical" label="Medical" />
+                  </Input>
+                  <ErrorMessage
+                    component="div"
+                    name="category"
+                    className="invalid-feedback"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="notes">Notes if any</Label>
+                  <Input type="textarea" name="text" id="notes" />
+                </FormGroup>
+                <Button color="success" type="submit">
+                  {this.props.buttonVal}
+                </Button>{" "}
+                <Button
+                  color="success"
+                  style={{
+                    backgroundColor: "white",
+                    border: "2px solid #03a679",
+                    color: "#03a679"
+                  }}
+                >
+                  Reset all
               </Button>
-            </Form>
-          )}
+              </Form>
+            )}
         </Formik>
       </div>
     );
